90bc0279de7c0462fc722a8db47fee0a45252f70
Michal <michalobert@zoznam.sk>
2015-11-07 15:36:55 +0100

    removed unnecesary files

---
+++ New Unity Project/Assets/BallScript.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+	}
+    void OnCollisionEnter(Collision col) {
+        Debug.Log("collision detected. Name: " + col.gameObject.name);
+    }
+    // Update is called once per frame
+    void Update () {
+	
+	}
+}
---
+++ New Unity Project/Assets/Scripts/LeapMotionListener.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using Leap;
+using System;
+
+public class LeapMotionListener : Listener {
+
+    //public delegate void ChangedEventHandler(object sender, EventArgs e);
+    //public event ChangedEventHandler Changed;
+
+   /* protected virtual void OnChanged(EventArgs e) {
+        if(Changed != null)
+            Changed(this, e);
+    }*/
+        public override void OnFrame(Controller controller) {
+            Frame a = controller.Frame();
+            Debug.Log("newFrame");
+            foreach(Gesture g in a.Gestures()) {
+                if(g.Type == Gesture.GestureType.TYPE_KEY_TAP) {
+                    Debug.Log("working");
+                    Vector3 positionOfTap = new Vector3(((KeyTapGesture)g).Position.x,((KeyTapGesture)g).Position.y,((KeyTapGesture)g).Position.z);
+             //       PositionOfTapEventArgs positionOfTapEventArgs = new PositionOfTapEventArgs();
+              //      positionOfTapEventArgs.PositionOfTap = positionOfTap;
+                //    OnChanged(positionOfTapEventArgs);
+                }
+            }
+        }
+
+
+}
+public class PositionOfTapEventArgs : EventArgs {
+    public Vector3 PositionOfTap;
+}
